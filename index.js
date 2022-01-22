df3630ed0189b6833ed94328ff1bff89376d9e69
Richard Walsh <richwalsh2@googlemail.com>
2022-01-22 19:17:49 +0000

    add mongoose and nodemon

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require('express');
-const res = require('express/lib/response');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const app = express();
+
+const accomodationRoutes = require('./routes/accommodation');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const db = mongoose.connection;
+
+if (!db) {
+  console.log('Error connecting to db');
+} else {
+  console.log('connected to db');
+}
 
 let port = process.env.port || 8080;
 
-app.get('/', (req, res) => res.send('HELLOW FROM THE SERVER'));
+app.get('/', (req, res) => res.send('HELLOW FROM THE SERVER nodemon'));
 
 app.listen(port, function () {
   console.log(`running server on port ${port}`);
 });
+
+app.use('/accomodation', accomodationRoutes);
--- routes/accommodation.js
+++ routes/accommodation.js
@@ -6,3 +6,11 @@ router.get('/', (request, response) => {
     message: 'Welcome to em adventures',
   });
 });
+
+// router.get('/test', (request, response) => {
+//   response.json({
+//     status: 'API IS WORKING',
+//     message: 'Welcome to test',
+//   });
+// });
+module.exports = router;
